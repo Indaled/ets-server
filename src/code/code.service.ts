fbeb87653d5989b47b5d984fc6343e6b9dfa85d1
indaled <info@indaled.ru>
2022-02-21 20:21:06 +0500

    Fix query

--- src/code/code.service.ts
+++ src/code/code.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {InjectModel} from "@nestjs/sequelize";
 import {Code} from "./code.model";
-import {Op} from "sequelize";
+import {Op, QueryTypes} from "sequelize";
 
 @Injectable()
 export class CodeService {
@@ -27,13 +27,13 @@ export class CodeService {
         return cross_ids
     }
     async searchCrossIdsLike(s: string) {
-        const cross_ids = await this.codeRepository.findAll({
-            where: {
-                part_number: {
-                    [Op.like]: `%${s}%`
-                }
-            },
-        })
+        const cross_ids = await this.codeRepository.sequelize.query(`SELECT c.* FROM crosses AS c WHERE c.cross_id IN ( SELECT DISTINCT cc.cross_id FROM crosses AS cc WHERE cc.part_number LIKE :search_name )`,
+            {
+                model: Code,
+                mapToModel: true,
+                replacements: { search_name: `%${s}%` },
+                type: QueryTypes.SELECT
+            });
         return cross_ids
     }
 }
